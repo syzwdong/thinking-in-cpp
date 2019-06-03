a5f4ccf3cb3bbfa2366f628ed5cdbfae04217d41
liph <3138519115@qq.com>
2019-06-03 17:06:43 +0800

    chapter 6

--- code/2-6-algorithm9.cpp
+++ code/2-6-algorithm9.cpp
@@ -18,12 +18,12 @@ int main()
     //创建小顶堆
     make_heap(data.begin(), data.end(), greater<>());
     print(data);
-    //=>4 5 13 26 26 14 18 47 
+    //=>4 5 13 26 14 18 47 26 
 
     //创建大顶堆
     make_heap(data.begin(), data.end());
     print(data);
-    //=>47 26 18 26 4 14 13 5 
+    //=>47 26 18 26 14 4 13 5 
 
     //添加
     data.push_back(37);
@@ -32,18 +32,20 @@ int main()
     //=>47 37 18 26 14 4 13 5 26
 
     //删除
-    pop_heap(begin(data), end(data),greater<>());
+    pop_heap(begin(data), end(data));
     data.pop_back();
     print(data);
+    //=>37 26 18 26 14 4 13 5
 
     //判断是否依旧为堆
-    cout<<is_heap(begin(data),end(data))<<endl;
-    cout<<is_heap(begin(data),end(data),greater<>())<<endl;
+    cout << is_heap(begin(data), end(data)) << endl;
+    //=>1
 
     //排序
-    if(is_heap(begin(data),end(data),greater<>()))
-        sort_heap(begin(data),end(data),greater<>());
+    if(is_heap(begin(data), end(data)))
+        sort_heap(begin(data), end(data));
     print(data);
-
+    //=>4 5 13 14 18 26 26 37
+    
     return 0;
 }
